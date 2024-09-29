054f8afcf221455b6d1bd0fc01b24a58af13176d
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-29 08:06:56 -0500

    Update header.js

--- import/header.js
+++ import/header.js
@@ -5,8 +5,8 @@ document.write(`
     <a href="newest.html/">Newest Sounds</a>
     <a href="oldest.html/">Oldest Sounds</a>
     <a href="other.html">Other Links</a>
-    <a href="https://discord.gg/zhsmmNNtJT"><i class="fab fa-discord"></i></a>
-    <a href="https://github.com/CybriaTech/CybriaSoundboard"><i class="fab fa-github"></i></a>
+    <a href="https://discord.gg/zhsmmNNtJT" target="_blank"><i class="fab fa-discord"></i></a>
+    <a href="https://github.com/CybriaTech/CybriaSoundboard" target="_blank"><i class="fab fa-github"></i></a>
   </nav>
   <div id="right">
         <form id="form-top" action="search.html" method="get">
