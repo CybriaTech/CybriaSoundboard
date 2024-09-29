25faee4052559d66b5820e1f9ea6ed71bddc841d
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-29 08:03:47 -0500

    Create header.js

---
+++ import/header.js
@@ -0,0 +1,16 @@
+document.write(`
+<header>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/newest.html/">Newest Sounds</a>
+    <a href="/oldest.html/">Oldest Sounds</a>
+    <a href="other.html">Other Links</a>
+  </nav>
+  <div id="right">
+        <form id="form-top" action="search.html" method="get">
+      <input type="text" name="q" class="search-bar-top" placeholder="Search For Sounds">
+      <button class="search-enter-top"><i class="fas fa-search"></i></button>
+      </form>
+  </div>
+</header>
+`);
