062b5815242d1c96476b2059c8d9d1078ed4e19b
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-10-06 08:21:01 -0500

    Update sounds.js

--- import/sounds.js
+++ import/sounds.js
@@ -1,10 +1,15 @@
+function rng() {
+    return Math.floor(Math.random() * 10000);
+}
+
 function genbtn(sound) {
+    const audioid = rng();
     const soundcomponent = document.createElement('div');
     soundcomponent.classList.add('sound');
 
     const soundbtn = document.createElement('div');
     soundbtn.classList.add('soundbtn');
-    soundbtn.setAttribute('onclick', `this.play();`);
+    soundbtn.setAttribute('onclick', `document.getElementById('${audioid}').play();`);
 
     const title = document.createElement('p');
     title.classList.add('soundtitle');
