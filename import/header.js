8f6ef3049351e4b7ef762cc3b1dcf7052dea92f2
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-10-09 19:13:34 -0500

    Update header.js

--- import/header.js
+++ import/header.js
@@ -8,7 +8,7 @@ document.write(`
     <a href="https://discord.gg/zhsmmNNtJT" target="_blank"><i class="fab fa-discord"></i></a>
     <a href="https://github.com/CybriaTech/CybriaSoundboard" target="_blank"><i class="fab fa-github"></i></a>
     <a href="widgetbot.html" target="_blank"><i class="fas fa-comment"></i></a>
-    <a onclick="settings()"><i class="fas fa-cog"></i></a>
+    <a onclick="document.getElementById('settings').style.display = 'flex';"><i class="fas fa-cog"></i></a>
   </nav>
   <div id="right">
         <form id="form-top" action="q.html" method="get">
